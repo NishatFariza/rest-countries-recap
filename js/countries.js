2fcf27101bd010d0ef3459d95503fd012581481b
NishatFariza <nishatfarzia78@gmail.com>
2022-03-14 22:13:53 +0600

    data fetch

--- js/countries.js
+++ js/countries.js
@@ -1,11 +1,13 @@
 const loadCountries = () =>{
-    const url =`https://restcountries.com/v3.1/all`;
+    const url =(`https://restcountries.com/v3.1/all`)
     fetch(url)
-    .then(res =>res.json())
-    .then(data =>displayCountries(data))
+    .then(res => res.json())
+    .then(data => displayCountries(data))
+    
 }
 
 const displayCountries = (countries) =>{
    console.log(countries);
-};
-loadCountries()
+}
+
+loadCountries();
