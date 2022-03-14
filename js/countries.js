6bb41902885152f94386bde2c3165c7269a35a75
NishatFariza <nishatfarzia78@gmail.com>
2022-03-14 21:42:04 +0600

    initial set up

---
+++ js/countries.js
@@ -0,0 +1,11 @@
+const loadCountries = () =>{
+    const url =`https://restcountries.com/v3.1/all`;
+    fetch(url)
+    .then(res =>res.json())
+    .then(data =>displayCountries(data))
+}
+
+const displayCountries = (countries) =>{
+   console.log(countries);
+};
+loadCountries()
