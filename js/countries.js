875c67c7160fe31bdc5ceb35d34e549fb8fa9a98
NishatFariza <nishatfarzia78@gmail.com>
2022-03-14 22:45:24 +0600

    all country name show in ui

--- js/countries.js
+++ js/countries.js
@@ -8,6 +8,20 @@ const loadCountries = () =>{
 
 const displayCountries = (countries) =>{
    console.log(countries);
+     const allCountriesHTML= countries.map(country => getCountryHTML(country));
+//    console.log(allCountriesHTML[0]);
+     const container = document.getElementById('countries')
+     container.innerHTML =allCountriesHTML.join(' ');
+   
+}
+
+const getCountryHTML = country => {
+    return `
+       <div class="country">
+         <h2>${country.name.common}</h2>
+         <img src="${country.flags.png}"/>
+       </div>
+    `
 }
 
 loadCountries();
