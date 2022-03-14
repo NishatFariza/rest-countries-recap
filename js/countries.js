3d76f4869aa8372e4ae03c47a4261e88643e73e8
NishatFariza <nishatfarzia78@gmail.com>
2022-03-15 00:40:55 +0600

    object parameter destructing

--- js/countries.js
+++ js/countries.js
@@ -15,13 +15,39 @@ const displayCountries = (countries) =>{
    
 }
 
-const getCountryHTML = country => {
+//option 2
+//parameter destructing
+const getCountryHTML = ({name, flags}) => {
+    // const {name, flags} = country;
     return `
        <div class="country">
-         <h2>${country.name.common}</h2>
-         <img src="${country.flags.png}"/>
+         <h2>${name.common}</h2>
+         <img src="${flags.png}"/>
        </div>
     `
 }
+
+
+// option 1
+// const getCountryHTML = country => {
+//     //option 1
+//     const {name, flags} = country;
+//     return `
+//        <div class="country">
+//          <h2>${name.common}</h2>
+//          <img src="${flags.png}"/>
+//        </div>
+//     `
+// }
+
+//original
+// const getCountryHTML = country => {
+//     return `
+//        <div class="country">
+//          <h2>${country.name.common}</h2>
+//          <img src="${country.flags.png}"/>
+//        </div>
+//     `
+// }
 
 loadCountries();
